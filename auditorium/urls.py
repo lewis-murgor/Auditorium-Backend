fae02959b8158e110915b0a5fa52dd764b3e6146
Lewis <kiplagatlewis29@gmail.com>
2025-08-07 00:06:49 +0300

    create shows url

--- auditorium/urls.py
+++ auditorium/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('shows/', include('shows.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
